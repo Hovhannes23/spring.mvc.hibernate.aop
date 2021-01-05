02daddf4df826b08164260883c3707729451a900
Hovhannes23 <twinkle.groupbz@gmail.com>
2021-01-05 19:05:34 +0300

    transaction openning fixed

--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/controller/MyController.java
@@ -5,6 +5,7 @@ import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,7 @@ public class MyController {
     private EmployeeDAO employeeDAO;
 
     @RequestMapping("/")
+    @Transactional
     public String showAllEmployees(Model model){
 
        List<Employee> allEmployees = employeeDAO.getAllEmployees();
